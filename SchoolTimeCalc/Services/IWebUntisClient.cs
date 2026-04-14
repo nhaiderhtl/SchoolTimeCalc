dedfbf2ebec6b6a019a64e9107e3d68d600e8e40
nhaiderhtl <n.haider@students.htl-leonding.ac.at>
2026-04-14 13:58:10 +0200

    feat(02-04): add webuntis data and logout endpoints

--- SchoolTimeCalc/Services/IWebUntisClient.cs
+++ SchoolTimeCalc/Services/IWebUntisClient.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using System.Threading.Tasks;
 using Refit;
 
@@ -7,5 +8,20 @@ namespace SchoolTimeCalc.Services
     {
         [Post("/WebUntis/jsonrpc.do")]
         Task<UntisRpcResponse<UntisAuthResponse>> AuthenticateAsync([Body] UntisRpcRequest request, [Query] string school);
+
+        [Post("/WebUntis/jsonrpc.do")]
+        Task<UntisRpcResponse<JsonElement>> GetSubjectsAsync([Body] UntisRpcRequest request, [Query] string school, [Header("Cookie")] string cookie);
+
+        [Post("/WebUntis/jsonrpc.do")]
+        Task<UntisRpcResponse<JsonElement>> GetTeachersAsync([Body] UntisRpcRequest request, [Query] string school, [Header("Cookie")] string cookie);
+
+        [Post("/WebUntis/jsonrpc.do")]
+        Task<UntisRpcResponse<JsonElement>> GetRoomsAsync([Body] UntisRpcRequest request, [Query] string school, [Header("Cookie")] string cookie);
+
+        [Post("/WebUntis/jsonrpc.do")]
+        Task<UntisRpcResponse<JsonElement>> GetTimetableAsync([Body] UntisRpcRequest request, [Query] string school, [Header("Cookie")] string cookie);
+
+        [Post("/WebUntis/jsonrpc.do")]
+        Task<UntisRpcResponse<object>> LogoutAsync([Body] UntisRpcRequest request, [Query] string school, [Header("Cookie")] string cookie);
     }
 }
