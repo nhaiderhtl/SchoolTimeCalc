6e5d9b02099ee36ba4178821a38fff2ea6bb8736
nhaiderhtl <n.haider@students.htl-leonding.ac.at>
2026-04-14 13:18:47 +0200

    feat(01-01): scaffold Blazor Web App structure
    - Convert to Web SDK
    - Add basic Razor component routing
    - Add Program.cs configuration

--- SchoolTimeCalc/Program.cs
+++ SchoolTimeCalc/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseAntiforgery();
+
+// Ensure static files can be served
+app.UseStaticFiles();
+
+app.MapRazorComponents<SchoolTimeCalc.Components.App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
