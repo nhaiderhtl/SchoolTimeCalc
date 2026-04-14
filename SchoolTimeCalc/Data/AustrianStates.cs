2f4956aca47619d54333eb595386f9023da3f113
nhaiderhtl <n.haider@students.htl-leonding.ac.at>
2026-04-14 13:27:37 +0200

    feat(01-03): build Settings component and layout with state persistence
    - Created AustrianStates static list
    - Implemented Settings.razor with EditForm to save Bundesland
    - Added MainLayout and NavMenu with Tailwind CSS classes
    - Implemented Home.razor to enforce state selection redirect
    - Configured Routes to use MainLayout

---
+++ SchoolTimeCalc/Data/AustrianStates.cs
@@ -0,0 +1,17 @@
+namespace SchoolTimeCalc.Data;
+
+public static class AustrianStates
+{
+    public static readonly string[] States = new[]
+    {
+        "Wien",
+        "Niederösterreich",
+        "Burgenland",
+        "Salzburg",
+        "Steiermark",
+        "Oberösterreich",
+        "Kärnten",
+        "Tirol",
+        "Vorarlberg"
+    };
+}
